a569707fbd43ee83adc42bbe00b10d548c9e354e
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-10-06 17:24:59 +0300

    computed values

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container">
 
-    <div class="card mt-6 mr-6">
+    <div v-show="inProgressAssignments.length" class="card mt-6 mr-6">
       <div class="card-body mx-auto">
         <p><strong> In Progress Assignments </strong></p>
         <div class="mb-3">
           <ul>
-            <li v-for="assignment in assignments.filter(a=>!a.complete)">
+            <li v-for="assignment in inProgressAssignments">
               <label>
                 {{ assignment.name }}
                 <input type="checkbox" v-model="assignment.complete" :key="assignment.id">
@@ -18,12 +18,12 @@
     </div>
 
 
-    <div class="card mt-6 mr-6">
+    <div v-show="completeAssignments.length" class="card mt-6 mr-6">
       <div class="card-body mx-auto">
         <p><strong> Completed </strong></p>
         <div class="mb-3">
           <ul>
-            <li v-for="assignment in assignments.filter(a => a.complete)">
+            <li v-for="assignment in completeAssignments">
               <label>
                 {{ assignment.name }}
                 <input type="checkbox" v-model="assignment.complete" :key="assignment.id">
@@ -48,6 +48,15 @@ export default {
         {name: "Conduct lab experiment", complete: false, id:2},
         {name: "Research on the Thesis", complete: false, id:3}
       ]
+    }
+  },
+
+  computed:{
+    completeAssignments(){
+      return this.assignments.filter(a=>a.complete);
+    },
+    inProgressAssignments(){
+      return this.assignments.filter(a=>!a.complete);
     }
   },
 
