89d81dd9e421bc27bec5ad6525214d7620db0646
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-10-17 17:17:43 +0300

    Lazy loading about page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,11 @@
 import {createRouter, createWebHistory} from "vue-router";
-import About from "@/pages/About.vue";
 import Home from '@/pages/Home.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path:'/', name:'home', component:Home},
-        {path:'/about', name:'about', component:About},
+        {path:'/about', name:'about', component:()=> import('@/pages/About.vue')},
 
     ]
 })
